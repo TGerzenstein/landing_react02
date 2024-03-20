528f8649c55edf1238c3bf621bc772c14b3ba566
Tatiana <tatigerzens@gmail.com>
2024-03-20 10:49:54 -0300

    added new class in CSS

--- components/SearchComponent.jsx
+++ components/SearchComponent.jsx
@@ -1,58 +1,42 @@
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 
-export default function SearchComponent({ searchText, handleSearch }) {
+export default function SearchComponent({ searchText }) {
   
   const [ productos, setProductos ] = useState([]);
-  const [ search, setSearch ] = useState('');
   
   async function showData(searchText) {
     const url = `https://api.mercadolibre.com/sites/MLA/search?q=${searchText}`
     const respuesta = await fetch(url) 
     const data = await respuesta.json();
-
-    //console.log(data.results)
-    //return data.results
+ 
+    console.log("mostrar:", data.results)
     setProductos(data.results);
   }
-  //showData();
+
 
   useEffect(() => {
     showData(searchText);
   }, [searchText])
+  console.log("componente ejecutado");
 
-  //const handleSearch = (event) => {
-    //setSearch(event.target.value);
-  //  searchText(event.target.value);
-//  }
-
-  // filtrado
-  //let entradas = []
-  
- // if(!search) 
-  //{
-   // entradas = productos
-  //} else {
-    //    entradas = productos.filter( (dato) => 
-    //    dato.title.toLowerCase().includes(search.toLocaleLowerCase()) 
-    //)        
-  //}
 
   return (
     <section>
       <article className='container-products'>
         {productos.map((item) => (
-          
-          <div  className='card-product' key={item.id}>
+          <Link className='card-product' key={item.id} href={`/items/${item.id}`}>
             <img src={item.thumbnail} alt={item.title}/>
                 <div className='line'></div>
                 <div className='info-product'>
-                <p>{item.condition}</p>
-                <a className="an"href=""><h2 className='title-product'>{item.title}</h2></a>
-                  <p className='price'>${item.price}</p>
-                  <p>{item.currency_id}</p>
+                <p className='condition'>{item.condition}</p>
+                <a className="an" href=""><h2 className='title-product'>{item.title}</h2></a>
+                  <p className='price'>$ {Intl.NumberFormat("de-DE").format(item.price)}{" "} {item.currency_id}</p>
+                  <p className='title-stock'>Stock disponible</p>
+                  <p className='stock'>Cantidad: {item.available_quantity}</p>
                 </div>
-          </div>
+          </Link>
         ))}
       </article>
     </section>
