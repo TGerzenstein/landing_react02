2e42a30e30bcfac925fb7d15404d4934757c3314
Tatiana <tatigerzens@gmail.com>
2024-03-22 10:40:18 -0300

    Modified styles within media queries for product details

--- src/app/items/[id]/page.jsx
+++ src/app/items/[id]/page.jsx
@@ -22,23 +22,19 @@ export default function SearchId({ params }) {
   return (
       <section className='section-product'>
         <div className="container">
-          <div className='product-container'>
-              <div className="box-1">
-                <img src={productDetails.thumbnail} alt={productDetails.title} className='img-product'/>
-              </div>
-              <div className="box-3">
-                <div className="line-product"></div>
-                <ProductDescriptionText productId={params.id} />
-              </div>
-          </div>
-          <div className='banner'>
+            <div className="box-1">
+              <img src={productDetails.thumbnail} alt={productDetails.title} className='img-product'/>
+            </div>
             <div className='box-2'>
-                  <p className="condition-product">{productDetails.condition}</p> 
-                  <p className='product-title'>{productDetails.title}</p>
-                  <p  className='product-price'>$ {Intl.NumberFormat("de-DE").format(productDetails.price)}{" "} {productDetails.currency_id}</p>
-                  <p className="options">Ver los medios de pagos</p>
-            </div>    
-          </div>
+                <p className="condition-product">{productDetails.condition}</p> 
+                <p className='product-title'>{productDetails.title}</p>
+                <p  className='product-price'>$ {Intl.NumberFormat("de-DE").format(productDetails.price)}{" "} {productDetails.currency_id}</p>
+                <p className="options">Ver los medios de pagos</p>
+            </div> 
+            <div className="box-3">
+              <div className="line-product"></div>
+              <ProductDescriptionText productId={params.id} />
+            </div>   
         </div>
       </section>
   );
